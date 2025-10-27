309c3aed3d17125d5e159595727bc4bf1bff38b5
Jhe <kwinjherane.ocareza@clsu2.edu.ph>
2025-10-28 07:11:58 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "127.0.0.1:3006";
+$servername = "127.0.0.1";
 $username = "root";
-$password = "aespa";
+$password = "";
 $database = "accounting_system";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
