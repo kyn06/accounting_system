131505ae4317b5d68f5005cf781a2c1c5de2b073
Jhe <kwinjherane.ocareza@clsu2.edu.ph>
2025-10-28 21:54:25 +0800

    .

--- db.php
+++ db.php
@@ -3,9 +3,8 @@ $servername = "127.0.0.1";
 $username = "root";
 $password = "";
 $database = "accounting_system";
-$root = "";
 
-$conn = mysqli_connect($servername, $username, $password, $database, $root);
+$conn = mysqli_connect($servername, $username, $password, $database);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
